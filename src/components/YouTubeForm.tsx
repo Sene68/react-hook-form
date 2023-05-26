afa416fc99e9867b82d11aecab94e5a43e25d212
Sene68 <senefull68@gmail.com>
2023-05-27 02:04:07 +0900

    devtool 추가

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,9 +1,10 @@
 import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools"
 
 export const YouTubeForm = () => {
 
     const form = useForm()
-    const { register } = form
+    const { register, control } = form
 
     return(
         <div>
@@ -19,6 +20,7 @@ export const YouTubeForm = () => {
 
                 <button>Submit</button>
             </form>
+            <DevTool control={control} />
         </div>
     )
 }
