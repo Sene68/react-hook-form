e3c1578a0e7a1218b79ab7011480571e48d2b6fb
Sene68 <senefull68@gmail.com>
2023-06-03 00:45:05 +0900

    load saved data

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -12,10 +12,14 @@ type FormValues = {
 export const YouTubeForm = () => {
 
     const form = useForm<FormValues>({
-        defaultValues: {
-            username: "Batman",
-            email: "",
-            channel: "",
+        defaultValues: async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+            const data = await response.json()
+            return {
+                username: "Batman",
+                email: data.email,
+                channel: ""
+            }
         },
     })
     const { register, control, handleSubmit, formState } = form
