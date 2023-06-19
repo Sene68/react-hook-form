3de846927be2047160568ea6f9191ccf91c7e7b5
Sene68 <senefull68@gmail.com>
2023-06-19 18:56:23 +0900

    submit count

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -42,9 +42,9 @@ export const YouTubeForm = () => {
         },
     })
     const { register, control, handleSubmit, formState, watch, getValues, setValue } = form
-    const { errors, touchedFields, dirtyFields, isDirty, isValid, isSubmitting, isSubmitted, isSubmitSuccessful } = formState
+    const { errors, touchedFields, dirtyFields, isDirty, isValid, isSubmitting, isSubmitted, isSubmitSuccessful, submitCount } = formState
 
-    console.log({ isSubmitting, isSubmitted, isSubmitSuccessful })
+    console.log({ isSubmitting, isSubmitted, isSubmitSuccessful, submitCount })
     // console.log(touchedFields, dirtyFields, isDirty, isValid)
 
     const { fields, append, remove } = useFieldArray({
